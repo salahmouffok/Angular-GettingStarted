7b1c7f31cb72b5eeab6d5d6e509f2158e93cf8fe
Salaheddine MOUFFOK <salaheddine.mouffok@hotmail.com>
2021-07-16 08:25:37 +0200

    Module 7 finished

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,7 +12,18 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage = false;
-    listFilter: string = 'cart';
+    
+    _listFilter!: string ;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string){
+        this._listFilter = value;
+        this.filteredProducts = this.performFilter(this.listFilter);
+    }
+
+    filteredProducts: IProduct[];
     products : IProduct[] = [
         {
             "productId": 1,
@@ -33,8 +44,29 @@ export class ProductListComponent implements OnInit{
             "price": 32.99,
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
         }
     ];
+
+    constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter = "cart";
+    }
+
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().includes(filterBy))
+    }
 
     toggleImage(): void{
         this.showImage = ! this.showImage;
